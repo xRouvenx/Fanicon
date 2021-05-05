cff8b8fa34d6212d4f8c46996eea81b1f66dfc68
Cedric Gasser <cedric_gasser@sluz.ch>
2021-05-05 11:23:02 +0200

    Created Spring Boot Project

---
+++ backend/src/test/java/com/cedricgasser/fanicon/FaniconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cedricgasser.fanicon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaniconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
